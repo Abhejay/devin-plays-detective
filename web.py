bf40c8085a2e6006d49e9ec0dd5e13cc04604017
Abhejay <abhejay@hotmail.com>
2026-03-01 21:20:11 -0600

    New web.py changes

--- web.py
+++ web.py
@@ -1,54 +1,96 @@
 import pickle
 import yaml
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, escape
 import urllib.request
 from lxml import etree
+import db
+import system
+import auth
 
 app = Flask(__name__)
 
 @app.route("/greet")
 def greet():
-    # XSS (CWE-79)
     name = request.args.get("name", "")
     return f"<h1>Welcome, {escape(name)}!</h1>"
 
 @app.route("/search")
 def search():
-    # XSS (CWE-79)
     query = request.args.get("q", "")
     return f"<p>Search results for: {escape(query)}</p>"
 
 @app.route("/redirect")
 def open_redirect():
-    # Open Redirect (CWE-601)
     url = request.args.get("url")
     return redirect(url)
 
 @app.route("/fetch")
 def fetch_url():
-    # SSRF (CWE-918)
     url = request.args.get("url")
     response = urllib.request.urlopen(url)
     return response.read()
 
 @app.route("/parse_xml", methods=["POST"])
 def parse_xml():
-    # XXE (CWE-611)
     xml_data = request.data
     parser = etree.XMLParser(resolve_entities=True, no_network=False)
     tree = etree.fromstring(xml_data, parser=parser)
     return etree.tostring(tree)
 
 def load_user_session(session_data):
-    # Insecure Deserialization (CWE-502)
     return pickle.loads(session_data)
 
 def parse_config(config_str):
-    # Insecure Deserialization (CWE-502)
     return yaml.load(config_str)
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    # Insecure Deserialization (CWE-502)
     data = pickle.loads(request.data)
     return str(escape(data))
+
+@app.route("/user")
+def get_user():
+    username = request.args.get("username", "")
+    return str(db.get_user(username))
+
+@app.route("/delete_user", methods=["POST"])
+def delete_user():
+    user_id = request.form.get("id", "")
+    db.delete_user(user_id)
+    return "Deleted"
+
+@app.route("/shop")
+def shop():
+    kw = request.args.get("kw", "")
+    return str(db.search_products(kw))
+
+@app.route("/ping")
+def ping():
+    host = request.args.get("host", "")
+    system.ping_host(host)
+    return "Pinged"
+
+@app.route("/info")
+def file_info():
+    filename = request.args.get("filename", "")
+    return system.get_file_info(filename)
+
+@app.route("/read")
+def read_file():
+    filepath = request.args.get("filepath", "")
+    return system.read_file(filepath)
+
+@app.route("/download")
+def download_file():
+    filename = request.args.get("filename", "")
+    return system.download_file(filename)
+
+@app.route("/login", methods=["POST"])
+def login():
+    pw = request.form.get("password", "")
+    return auth.hash_password(pw)
+
+@app.route("/fetch_data")
+def fetch_external_data():
+    url = request.args.get("url", "")
+    return auth.fetch_data(url)
